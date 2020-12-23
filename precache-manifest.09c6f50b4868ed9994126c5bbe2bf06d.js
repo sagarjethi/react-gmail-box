56bcdffa950f019450a4ea0e5d3fbe4e4178294a
sagar <sagar.jethi007@gmail.com>
2020-12-23 17:49:16 +0530

    Updates

---
+++ precache-manifest.09c6f50b4868ed9994126c5bbe2bf06d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859856e78bab0b698a39b4d74710707e",
+    "url": "/CubeMail/index.html"
+  },
+  {
+    "revision": "371d7f1f0d94b59dbb91",
+    "url": "/CubeMail/static/js/2.3207cc07.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/CubeMail/static/js/2.3207cc07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b707410f3f8d5eef6ded",
+    "url": "/CubeMail/static/js/main.78591164.chunk.js"
+  },
+  {
+    "revision": "e4d6d5a8b1e409ffc3ec",
+    "url": "/CubeMail/static/js/runtime-main.8b14a9d4.js"
+  },
+  {
+    "revision": "c9034baf5e3a91a16cd1d3ba9de5b925",
+    "url": "/CubeMail/static/media/empty_email.c9034baf.svg"
+  }
+]);
